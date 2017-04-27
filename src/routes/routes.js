31be9f84841b633cf437a62fa367126382235c3b
glennwedin <glenn.wedin@gmail.com>
2017-04-27 13:32:35 +0200

    cleanup

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,3 @@
-/*
-import React from "react";
-import {Router, Route} from "react-router";
-import MainComponent from "../containers/MainComponent";
-
-var mainroute = (history) => {
-	history = history || null;
-	return (
-			<Router history={history}>
-				<Route path="/" component={MainComponent} >
-				</Route>
-			</Router>
-		);
-};
-
-export default mainroute;
-*/
 import React from "react";
 import {Route, StaticRouter, BrowserRouter } from 'react-router-dom';
 import Main from "../containers/MainComponent";
